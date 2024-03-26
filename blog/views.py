2e13b9f3786f4caa0e5ef80684b3954494699128
Andrew Hanson <lah5598@gmail.com>
2024-03-25 18:08:37 -0700

    added database integration and created users app

--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,32 @@
 from django.shortcuts import render
+from .models import Post
 from django.http import HttpResponse
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018',
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 27, 2018',
-    },
-
-
-
-
-]
+# posts = [
+#     {
+#         'author': 'CoreyMS',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'August 27, 2018',
+#     },
+#     {
+#         'author': 'Jane Doe',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'August 27, 2018',
+#     },
+#
+#
+#
+#
+# ]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all(),
     }
     return render(request, 'blog/home.html', context)
 
