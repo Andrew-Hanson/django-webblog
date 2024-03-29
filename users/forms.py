6724bf6c1c4e5960b691160b306b16cc2e493cc6
Andrew Hanson <lah5598@gmail.com>
2024-03-29 16:05:51 -0700

    added user images next to posts

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -11,3 +13,20 @@ class UserRegisterForm(UserCreationForm):
         # fields that will be shown on the form
         # order of fields will be the order they are listed
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        # model that will be affected
+        model = User
+        # fields that will be shown on the form
+        # order of fields will be the order they are listed
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
