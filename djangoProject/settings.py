05211e1ade1ae432b117ac8abb0e7ffa42eeff60
Andrew Hanson <67885598+Andrew-Hanson@users.noreply.github.com>
2024-03-30 12:46:58 -0700

    Update settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -28,10 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['172.232.187.134', 'andrewhanson.dev' ]
-
+ALLOWED_HOSTS = ['172.232.187.134'] # 'andrewhanson.dev' ]
 
 # Application definition
 
@@ -127,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
